92de401b4b9c16df259a206c75955bfe0921caa8
Kedar Kshatriya <kshatriyakedar@gmail.com>
2024-07-21 00:49:56 +0000

    ui changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
         <div class="max-w-md">
-            <h1 class="text-5xl font-bold text-primary">Welcome to BasicPay!</h1>
+            <h1 class="text-5xl font-bold text-primary">Welcome to Capy Credit!</h1>
             <p class="py-6 text-secondary">
-                The app that lets you pay, <em>basically</em>, anyone.
+                The app that lets you create, <em>cedit score</em>.
             </p>
             <div data-sveltekit-preload-data="tap" data-sveltekit-preload-code="hover">
                 <a href="/signup" class="btn-primary btn">Signup Now</a>
